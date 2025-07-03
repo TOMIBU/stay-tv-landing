50f1b291020c8c0d5de19825ec3f2b3c09125477
Bau <bau_manolizi@outlook.com>
2025-07-02 21:15:33 -0300

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
+
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -40,7 +42,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-8NJLG9TW0Z"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8NJLG9TW0Z');
+            `,
+          }}
+        />
+        {children}
+      </body>
     </html>
   )
 }
